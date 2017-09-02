a43a01a3952b8d9ce63aea9d641d6a04ac895bf5
Sameer Ahmed.S <sameer.ahmed@mindtree.com>
2017-09-02 16:49:16 +0530

    Add project files.

---
+++ OwinFormAuthentication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OwinFormAuthentication.Startup))]
+namespace OwinFormAuthentication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
